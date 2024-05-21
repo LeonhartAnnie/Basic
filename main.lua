3fe23bf34ccfd22d93926670bb5bbcc3a0d5c715
LeonhartAnnie <122689484+LeonhartAnnie@users.noreply.github.com>
2024-05-21 18:08:46 +0800

    add enermy01

--- main.lua
+++ main.lua
@@ -5,7 +5,7 @@ local function setLandscapeMode()
     -- 將螢幕旋轉至橫向
     native.setProperty("Orientation","landscapeRight")
 
-    -- 設定螢幕的寬度和高度 x(-150,630) y(-35,355)
+    -- 設定螢幕的寬度和高度 x(-115,595) y(0,320)
     local width, height = display.contentWidth, display.contentHeight
 
     -- 更新場景中的內容，以適應新的螢幕尺寸
@@ -19,6 +19,15 @@ physics.start()
 --physics.setDrawMode( "hybrid" )
 --physics.setDrawMode( "debug" )
 
+local shape_1 = { -4.19999408721924,-13.9999971389771, 6.60000610351563,-13.5999975204468, 23.8000068664551,-5.19999742507935, 24.2000064849854,8.00000286102295, 17.4000072479248,14.8000030517578, -15.799994468689,14.8000030517578, -24.9999942779541,8.80000305175781, -24.5999946594238,-1.59999716281891 }
+shape_1.density = 1; shape_1.friction = 0.3; shape_1.bounce = 0.2; 
+
+local dispObj_1 = display.newImageRect( "images/sprite_1.png", 50, 29 )
+dispObj_1.x = -90
+dispObj_1.y = 305
+physics.addBody( dispObj_1, 
+    {density=shape_1.density, friction=shape_1.friction, bounce=shape_1.bounce, shape=shape_1}
+)
 local wall_Down = display.newImageRect( "images/ground.png", 710, 70 )
 wall_Down.x = 240
 wall_Down.y = 355 --355
@@ -43,11 +52,13 @@ wall_Up.rotation = -180
 physics.addBody( wall_Up, "static", { density=1, friction=0.3, bounce=0.2 } )
 
 --]]
--- 呼叫函數設置橫向模式
+
 local function Acount()
     print(1)
 end
+-- 呼叫函數設置橫向模式
 setLandscapeMode()
 timer.performWithDelay( 1000,Acount,5)
+
 
 
