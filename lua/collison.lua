fb15fabea00f0a8b320b89210b1f39475253cb43
zanya_owo <tea913636@gmail.com>
2024-05-24 22:47:40 +0800

    edit monster movement

--- lua/collison.lua
+++ lua/collison.lua
@@ -3,7 +3,8 @@ local speed_M = 7
 local direction_M = 1
 
 function collision.onCollision_M(event)
-    if event.phase == "ended" and event.other.id == "wall" then
+    if event.phase == "began" and (event.other.id == "wall" or event.other.id == "monster") then
+        event.contact.bounce = 1.5
         math.randomseed(os.time())
         direction_M = direction_M * (-1)
         speed_M = math.random(5,20)
@@ -16,7 +17,7 @@ function collision.onCollision_P(event, player)
     if event.other.id == "ground" then
         onGround = true
         --print(onGround)
-    elseif event.other.id == "monster" then
+    elseif event.other.id == "monster" and event.phase == "ended" then
         player.health = player.health-1
         print(player.health)
     end
