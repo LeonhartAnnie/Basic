4d96eaab91f1fc42e28d7ae4a8907b77b159a46f
LeonhartAnnie <122689484+LeonhartAnnie@users.noreply.github.com>
2024-05-25 01:51:04 +0800

    ??

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 -- 設定橫向模式
-package.path = package.path .. ";./lua/?.lua"
+package.path = package.path .. ";Basic/lua/?.lua"
 
 local movieclip = require("movieclip")
 local physics = require( "physics" )
@@ -45,7 +45,8 @@ Move_Right = false
 onGround = false
 
 
-timer.performWithDelay(5000, generator.addMonster, 0)
+local monster = generator.addMonster()
+--timer.performWithDelay(5000, generator.addMonster, 0)
 
 local function shoot(event)
     local fire = generator.addFire(player)
